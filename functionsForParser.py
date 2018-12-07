d60008db727194a1d8866f855253d92de0990cf6
Cyril Lepinette <lepinette.cyril@gmail.com>
2018-12-07 11:57:35 +0100

    adaptation des fonctions pour le parseur

--- functionsForParser.py
+++ functionsForParser.py
@@ -1,50 +1,10 @@
 import math;
 import numpy as np;
 
-doc1 = {};
-doc1["bonjour"]=1;
-doc1["appelle"]=2;
-doc1["michel"]=1;
-doc1["et"]=1;
-doc1["toi"]=1;
-doc1["comment"]=1;
-doc1["tu"]=1;
-print (doc1);
-
-doc2 = {};
-doc2["bonjour"]=1;
-doc2["appelle"]=1;
-doc2["Alfred"]=1;
-
-
-print (doc2);
-
-doc3 = [];
-
-
-docs = [];
-docs.append(doc1);
-docs.append(doc2);
-docs.append(doc3);
-print(docs);
-
-
-query1 = {}
-query1["appelle"]=1;
-query2 = {}
-query2["Alfred"]=2;
-
-queries =[]
-queries.append(query1)
-queries.append(query2)
-
-
-
 
 #TERM FREQUENCY:
 
 def term_frequency(word):
-
     if type(word) == int:
         if word <= 0:
             return 0;
@@ -53,53 +13,46 @@ def term_frequency(word):
     else:
         return 0;
 
-def score_documents_term_frequency(documents,query):
+def score_documents_term_frequency(documents,document,query):
     sum=0;
-    for document in documents:
-        for word in document:
-            if word == query:
-                print(str(query)+ " : "+str(term_frequency(document.get(query))));
-                sum+= term_frequency(document.get(query));
+    for key,values in documents.items():
+        if key == query:
+            temps = values.copy();
+            temps.pop(0);
+            for docs in temps:
+                if docs[0]==document:
+                    #print(str(query)+ " : "+str(term_frequency(docs[1])));
+                    sum+= term_frequency(docs[1]);
     return sum;
-
-
-print ("\n" + str(score_documents_term_frequency(docs,"appelle")));
-print ("\n" + str(score_documents_term_frequency(docs,"bonjour")));
-print (score_documents_term_frequency(docs,"Alfred"));
-
 
 
 #DOCUMENT FREQUENCY:
-def document_frequency(documents, word):
-    sum = 0;
-    for document in documents:
-        if word in document:
-            sum=sum+1;
-    return sum;
+def document_frequency(documents, query):
+    for key, values in documents.items():
+        if key == query:
+            return values[0];
+    return 0;
 
-def inverse_document_frequency(documents, word):
+def inverse_document_frequency(corpus, indexWords, query):
     '''
     if documents.__len__() == 0:
         return 0;
     '''
     #print("\ttaille document: "  +str(documents.__len__()) );
-    return math.log((documents.__len__()/document_frequency(documents,word)), 10)
+    return math.log((corpus.__len__()/document_frequency(indexWords,query)), 10)
 
 
-def tf_idf(documents, document, word):
-    #print("tf: "+  str(term_frequency(document.get(query))));
-    #print("idf: "+ str(inverse_document_frequency(documents,query)));
-    return term_frequency(document.get(word))  * inverse_document_frequency(documents,word);
+def tf_idf(corpus,indexWords, numDocument, query):
+    for key,values in indexWords.items():
+        if key == query:
+            temps = values.copy();
+            temps.pop(0);
+            for docs in temps:
+                if docs[0]==numDocument:
+                    return term_frequency(docs[1]) * inverse_document_frequency(corpus, indexWords, query);
 
-print ("\ndocument_frequency: " + str(document_frequency(docs,"appelle")));
-print ("document_frequency: " + str(document_frequency(docs,"Alfred")));
 
-print ("\nidf appelle: " + str(inverse_document_frequency(docs,"appelle")));
-print ("idf alfred: " + str(inverse_document_frequency(docs,"Alfred")) +"\n");
 
-print ("tf*idf: " + str(tf_idf(docs,doc1,"appelle")) +"\n" );
-print ("tf*idf: " + str(tf_idf(docs,doc1,"bonjour"))+"\n");
-print ("tf*idf: " + str(tf_idf(docs,doc1,"Alfred"))+"\n");
 
 
 def print_documents_to_tf_idf_vector(documents):
@@ -112,21 +65,14 @@ def print_documents_to_tf_idf_vector(documents):
     return  True;
 
 
-print_documents_to_tf_idf_vector(docs);
-#JUSQU'ICI OK
-
-
 #VECTOR & NORMALISATION:
 
-def vector_tf_idf_one_doc(documents,document):
+def vector_tf_idf_one_doc(corpus,document):
     vector = {}
-    for word in document:
+    for word in indexWords:
         vector[word]=tf_idf(documents,document,word);
     return vector;
 
-
-print("\nTF_IDF VECTOR:")
-print(vector_tf_idf_one_doc(docs, doc1))
 
 
 def l2_normalization_vector(document):
@@ -136,20 +82,6 @@ def l2_normalization_vector(document):
         sum = sum+values;
     vector_normalized = math.sqrt(sum);
     return vector_normalized
-
-print("\nNORMALISATION DU DOC 1: ")
-print(l2_normalization_vector(vector_tf_idf_one_doc(docs,doc1)))
-
-print("\nNORMALISATION DU DOC 2: ")
-print(l2_normalization_vector(vector_tf_idf_one_doc(docs,doc2)))
-
-
-
-print("\nNORMALISATION DE LA REQUETE \"ALFRED\": ")
-print(l2_normalization_vector(vector_tf_idf_one_doc(queries,query2)))
-
-print("\nNORMALISATION DE LA REQUETE \"APPELLE\": ")
-print(l2_normalization_vector(vector_tf_idf_one_doc(queries,query1)))
 
 
 #COSINE_SIMILARITY:
@@ -163,30 +95,5 @@ def cosine_similarity(arg_query, document,word):
     return (np.dot(tf_idf_query ,tf_idf_doc) / ( l2_norm_query * l2_norm_doc ) ) ;
 
 
-'''
-l2_norm_doc1=l2_normalization_vector(vector_tf_idf_one_doc(docs,doc1));
-l2_norm_doc2=l2_normalization_vector(vector_tf_idf_one_doc(docs,doc2));
 
-l2_norm_query1=l2_normalization_vector(vector_tf_idf_one_doc(queries,query1));
-l2_norm_query2=l2_normalization_vector(vector_tf_idf_one_doc(queries,query2));
-'''
-
-# PAS FINI !!!!
-print("\nCOSINE SIMILARITY ALFRED DANS DOC ALFRED: ")
-print(cosine_similarity(query2,doc2,"Alfred"));
-print("\nCOSINE SIMILARITY ALFRED DANS UN DOC QUI N'A PAS ALFRED: ")
-print(cosine_similarity(query2,doc1,"Alfred"));
-
-
-print("\nCOSINE SIMILARITY APPELLE QUERY1 DANS UN DOC 1: ")
-print(cosine_similarity(query1,doc1,"appelle"));
-
-print("\nCOSINE SIMILARITY APPELLE QUERY1 DANS UN DOC 2: ")
-print(cosine_similarity(query1,doc2,"appelle"));
-
-print("\nCOSINE SIMILARITY APPELLE QUERY2 DANS UN DOC 1: ")
-print(cosine_similarity(query2,doc1,"appelle"));
-
-print("\nCOSINE SIMILARITY APPELLE QUERY2 DANS UN DOC 2: ")
-print(cosine_similarity(query2,doc2,"appelle"));
 
--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 from fileLoader import indexCorpus
 from document import buildInvertedIndex
 
+from functionsForParser import *
+
 corpus = indexCorpus("corpus")
 
-words = buildInvertedIndex(corpus)
+indexWords = buildInvertedIndex(corpus)
 
 with open("output.txt", "w+") as file:
-    for word, doc in words.items():
+    for word, doc in indexWords.items():
         file.write("%s : %s \n" % (word, doc))
 
 
 
 #print (words)
-        
-
 #Wefeel : [2, (2188, 1), (2848, 1)]
-Wefeel : [Corpus, (Doc, nbOccurrence)]
+#Wefeel : [Corpus, (Doc, nbOccurrence)]
+#'Ocean': [3, (7, 1), (11, 1), (49, 1)],
+#'years.': [2, (34, 1), (57, 2)]
 
+print(indexWords)
+print("\n");
+print("Ocean a 1 occurence dans 3 documents")
+print("years a 3 occurence dans 2 documents")
+print("\nTerm_freq by document ");
+print ("\tOcean: " + str(score_documents_term_frequency(indexWords,7, "Ocean")));
+print ("\tyears: " + str(score_documents_term_frequency(indexWords,57, "years.")));
+#sdtf_ocean = score_documents_term_frequency(indexWords,7, "Ocean");
+#sdtf_year = score_documents_term_frequency(indexWords,57, "years.");
+
+print("\nDocument_frequency ");
+print ("\tOcean: " + str(document_frequency(indexWords, "Ocean")));
+print ("\tyears: " + str(document_frequency(indexWords, "years.")));
+
+print("\nInverse document_frequency ");
+print ("\tOcean: " + str(inverse_document_frequency(corpus,indexWords, "Ocean")));
+print ("\tyears: " + str(inverse_document_frequency(corpus, indexWords, "years.")));
+
+print("\ntf_idf");
+print ("\tOcean: " + str(tf_idf(corpus,indexWords,7, "Ocean")));
+print ("\tyears: " + str(tf_idf(corpus, indexWords,57, "years.")));
+
+print("\nvector_tf_idf");
+#print ("\tOcean: " + str(vector_tf_idf_one_doc(corpus,indexWords,7, "Ocean")));
+#print ("\tyears: " + str(vector_tf_idf_one_doc(corpus, indexWords,57, "years.")));
+
+print(vector_tf_idf_one_doc(corpus, 7))
