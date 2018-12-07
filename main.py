58ecb7bd8eeb0e0516f8ed7cb90d1dc54c833b8f
romgrelier <romain.grelier@outlook.com>
2018-12-07 11:42:38 +0100

    merge and and or added

--- main.py
+++ main.py
@@ -3,9 +3,44 @@ from document import buildInvertedIndex
 
 corpus = indexCorpus("corpus")
 
-words = buildInvertedIndex(corpus)
+index = buildInvertedIndex(corpus)
 
 with open("output.txt", "w+") as file:
-    for word, doc in words.items():
+    for word, doc in index.items():
         file.write("%s : %s \n" % (word, doc))
-        
+
+def merge_or(wordList, index):
+    commonDocument = set()
+
+    for word in wordList:
+
+        wordIndex = index[word][2:]
+        for doc, _ in wordIndex:
+            commonDocument.add(doc)
+
+    return commonDocument
+
+
+def merge_and(wordList, index):
+    common_document = merge_or(wordList, index)
+    
+    for word in wordList:
+        doc_presence = set()
+
+        wordIndex = index[word][2:]
+        for doc, _ in wordIndex:
+            doc_presence.add(doc)
+
+        common_document = common_document.intersection(doc_presence)
+ 
+    return common_document
+
+
+print("MERGE OR\n")
+commonDocument = merge_or(["rule", "spent", "revel"], index)
+
+print("MERGE AND\n")
+and_document = merge_and(["rule", "spent"], index)
+
+print(and_document)
+
